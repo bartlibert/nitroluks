25cb97a15a1ca2b39666686ef1176e86b8513b9a
Bart Libert <blibert@drgt.net>
2019-09-20 14:46:34 +0200

    fix: Add missing header guard

--- include/constants.h
+++ include/constants.h
@@ -1,4 +1,9 @@
+#ifndef NITROLUKSPP_CONSTANTS_H
+#define NITROLUKSPP_CONSTANTS_H
+
 namespace nitrolukspp {
 constexpr auto MAX_PIN_LENGTH = 20;
 constexpr auto MAX_PIN_INPUT_LENGTH = MAX_PIN_LENGTH + 1;
 } /* namespace nitrolukspp */
+
+#endif /* NITROLUKSPP_CONSTANTS_H */
